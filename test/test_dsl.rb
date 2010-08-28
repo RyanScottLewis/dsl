9ade115df4787fe0ebfe4dff63f49b1912d22d91
Ryan Lewis <c00lryguy@gmail.com>
2010-08-28 13:44:01 -0400

    alright lets see if this worked

--- test/test_dsl.rb
+++
@@ -1,7 +0,0 @@
-require 'helper'
-
-class TestDsl < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
