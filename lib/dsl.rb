92f7030330d1401d43ee269bc7f8376204990007
Ryan Lewis <c00lryguy@gmail.com>
2011-04-20 00:09:13 -0400

    changes

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -17,17 +17,3 @@ class DSL
     parent # Return the parent for convenience
   end
 end
-
-class Module
-  def dsl_method(opts)
-    raise(TypeError) unless opts.is_a?(Hash)
-    # For each dsl_method, define it in the class
-    # The methods do not accept arguments, only blocks
-    opts.each do |method, dsl_class|
-      define_method(method) do |&blk|
-        raise(ArgumentError, "method #{method} requires a block") unless !!blk
-        dsl_class.call(self, &blk)
-      end
-    end
-  end
-end
