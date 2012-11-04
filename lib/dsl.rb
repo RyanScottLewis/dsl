ecc697947b3f9673b4ca4fca9290e12695f2f0a8
Ryan Scott Lewis <ryan@rynet.us>
2012-11-04 01:07:57 -0400

    Remove old comments

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -1,12 +1,3 @@
-# module DSL
-# end
-# 
-# $LOAD_PATH.unshift(File.dirname(__FILE__))
-# 
-# require 'dsl/return_hash'
-# require 'dsl/delegate_instance_variables'
-
-
 class DSL
   
   class << self
